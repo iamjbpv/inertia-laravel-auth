aa56198436c86a2d83fedd694ff6adbba2ee0950
Jb <iamjbpv>
2020-10-30 01:08:21 +0800

    added laravel ui and vue ui auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [LoginController::class, 'showLoginForm'])
+    ->name('login')
+    ->middleware('guest');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
